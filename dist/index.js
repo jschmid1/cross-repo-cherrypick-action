2d2a475d31b060ac21521b5eda0a78876bbae94e
Joshua Schmid <jaiks@posteo.de>
2024-01-23 15:36:17 +0100

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -923,7 +923,7 @@ function replacePlaceholders(template, main, target, owner = "", repo = "") {
         .replace("${pull_author}", main.user.login)
         .replace("${pull_number}", main.number.toString())
         .replace("${pull_title}", main.title)
-        .replace("${pull_description", (_a = main.body) !== null && _a !== void 0 ? _a : "")
+        .replace("${pull_description}", (_a = main.body) !== null && _a !== void 0 ? _a : "")
         .replace("${target_branch}", target)
         .replace("${repo}", repo)
         .replace("${owner}", owner);
